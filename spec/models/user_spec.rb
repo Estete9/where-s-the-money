13dc3e02f796d8c562152507aaa550b65bb10639
Esteban <cepalacios@outlook.com>
2024-02-05 11:39:08 -0500

    TDD: implemment user with devise

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with a name, email, and password' do
+    user = FactoryBot.build(:user)
+    expect(user).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    user = FactoryBot.build(:user, name: nil)
+    expect(user).to_not be_valid
+  end
+
+  it 'is invalid without an email' do
+    user = FactoryBot.build(:user, email: nil)
+    expect(user).to_not be_valid
+  end
+
+  it 'is invalid without a password' do
+    user = FactoryBot.build(:user, password: nil)
+    expect(user).to_not be_valid
+  end
+
+
 end
