f7a46002b2b58a4aaa1c43acaf3b075ae58d97fb
Esteban <cepalacios@outlook.com>
2024-02-05 12:51:22 -0500

    refactor tests so it uses factory bot methods instead of directly calling it

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it 'is valid with a name, email, and password' do
-    user = FactoryBot.build(:user)
+    user = build(:user)
     expect(user).to be_valid
   end
 
   it 'is invalid without a name' do
-    user = FactoryBot.build(:user, name: nil)
+    user = build(:user, name: nil)
     expect(user).to_not be_valid
   end
 
   it 'is invalid without an email' do
-    user = FactoryBot.build(:user, email: nil)
+    user = build(:user, email: nil)
     expect(user).to_not be_valid
   end
 
