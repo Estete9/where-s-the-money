d1918437e5b0688244db585fa45a66ba73bf4df2
Esteban <cepalacios@outlook.com>
2024-02-06 12:08:23 -0500

    Implement link to activities#index passing the category_id

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,10 @@ class Category < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true
+  validates :icon, presence: true
+
+  def total_amount
+    # TODO calculate total amount from all transactions related to this category
+    "$25"
+  end
 end
