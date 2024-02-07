2433bce27fce1ddbb1863cf2adb09b36851c7906
Esteban <cepalacios@outlook.com>
2024-02-07 17:18:58 -0500

    Remove default value for category name in new category form

--- db/migrate/20240205170032_create_categories.rb
+++ db/migrate/20240205170032_create_categories.rb
@@ -1,7 +1,7 @@
 class CreateCategories < ActiveRecord::Migration[7.1]
   def change
     create_table :categories do |t|
-      t.string :name,   null: false, default: "New Category"
+      t.string :name,   null: false, default: ""
       t.string :icon, null: false, default: ""
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -31,7 +31,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_06_172327) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name", default: "New Category", null: false
+    t.string "name", default: "", null: false
     t.string "icon", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
