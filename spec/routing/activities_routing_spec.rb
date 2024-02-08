2172ef6fa0e8a45cf185b561bb86ce246915e36f
Esteban <cepalacios@outlook.com>
2024-02-07 22:50:52 -0500

    Implement tests for routing\activities_routing

--- spec/routing/activities_routing_spec.rb
+++ spec/routing/activities_routing_spec.rb
@@ -3,36 +3,36 @@ require "rails_helper"
 RSpec.describe ActivitiesController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(get: "/activities").to route_to("activities#index")
+      expect(get: "/categories/1/activities").to route_to("activities#index", category_id: "1")
     end
 
     it "routes to #new" do
-      expect(get: "/activities/new").to route_to("activities#new")
+      expect(get: "/categories/1/activities/new").to route_to("activities#new", category_id: "1")
     end
 
     it "routes to #show" do
-      expect(get: "/activities/1").to route_to("activities#show", id: "1")
+      expect(get: "categories/1/activities/1").to route_to("activities#show", category_id: "1", id: "1")
     end
 
     it "routes to #edit" do
-      expect(get: "/activities/1/edit").to route_to("activities#edit", id: "1")
+      expect(get: "categories/1/activities/1/edit").to route_to("activities#edit", category_id: "1", id: "1")
     end
 
 
     it "routes to #create" do
-      expect(post: "/activities").to route_to("activities#create")
+      expect(post: "categories/1/activities").to route_to("activities#create", category_id: "1")
     end
 
     it "routes to #update via PUT" do
-      expect(put: "/activities/1").to route_to("activities#update", id: "1")
+      expect(put: "categories/1/activities/1").to route_to("activities#update", category_id: "1", id: "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(patch: "/activities/1").to route_to("activities#update", id: "1")
+      expect(patch: "categories/1/activities/1").to route_to("activities#update", category_id: "1", id: "1")
     end
 
     it "routes to #destroy" do
-      expect(delete: "/activities/1").to route_to("activities#destroy", id: "1")
+      expect(delete: "categories/1/activities/1").to route_to("activities#destroy", category_id: "1", id: "1")
     end
   end
 end
